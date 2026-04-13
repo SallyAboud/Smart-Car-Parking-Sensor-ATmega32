413221422d3ea16e9b899c9bdefeaaefe6d7a5ee
SallyAboud <130311851+SallyAboud@users.noreply.github.com>
2026-04-14 00:01:12 +0200

    Upload source code for Smart Car Parking Sensor

---
+++ Smart car parking sensor/Debug/miniproject4.d
@@ -0,0 +1,12 @@
+miniproject4.o miniproject4.o: ../miniproject4.c ../Ultrasonic.h \
+  ../std_types.h ../lcd.h ../leds.h ../buzzer.h
+
+../Ultrasonic.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../leds.h:
+
+../buzzer.h:
